0eb280ee44c438aca97e4191426a884eff8e626a
bisanjabareen <bisan.yousef25@gmail.com>
2026-02-15 01:49:36 +0200

    fixed place holder

--- calloc/calloc.c
+++ calloc/calloc.c
@@ -3,6 +3,9 @@
 
 void* c_calloc(size_t size) {
     void* ptr = c_malloc(size);
-    *(char*)ptr &= 0;
+    char* itr = ptr;
+    for (size_t i = 0; i < size; i++) {
+        *(itr++) = 0;
+    }
     return ptr;
 }
